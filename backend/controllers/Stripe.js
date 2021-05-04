17706876635fbb08fe4baef512e3c33dd358eef2
ZoldekSama <zoldeksama@gmail.com>
2021-05-04 13:24:29 +0200

    Controller: Stripe, architecture done

--- backend/controllers/Stripe.js
+++ backend/controllers/Stripe.js
@@ -1,1 +1,17 @@
+const SECRET_API_KEY = process.env.SECRET_API_KEY;
+const stripe = require('stripe')(SECRET_API_KEY);
 
+
+module.exports = {
+    
+    intents: {
+
+    },
+
+    payments: {
+
+    },
+    customers: {
+
+    },
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-const SECRET_API_KEY = process.env.SECRET_API_KEY;
 const StripeController = require("./controllers/Stripe");
 const express = require('express');
 const mongoose = require('mongoose')
-const stripe = require('stripe')(SECRET_API_KEY);
 const app = express();
 
 
