0aff4db7d45a1e851cd07cacdd67924132b61cc8
ZoldekSama <zoldeksama@gmail.com>
2021-05-04 16:46:31 +0200

    Payments - methods, created

--- backend/controllers/Stripe.js
+++ backend/controllers/Stripe.js
@@ -64,7 +64,90 @@ module.exports = {
     },
 
     payments: {
+        create: async (req, res, next) => {
+            try {
+                //! Moyen de paiement
+                const newPaymentMethod = await stripe.paymentMethods
+                .create({   
+                    billing_details: {
+                        email: "test@test.fr",
+                        name: "Iskariot",
+                        phone: "+33 858153874",
+                    },
+                    type: 'card',
+                    card: {
+                        number: "4242424242424242",
+                        exp_month: 2,
+                        exp_year: 2024,
+                        cvc: "190"
+                    },
+                });
+                
+                return res.json({ success: true, result: newPaymentMethod})
+        
+            } 
+            
+            catch (error) {
 
+                next(error);
+            }
+        },
+
+        savePaymentMethodToAnUser: async (req, res, next) => {
+            
+            try {
+                //! Méthode de paiement (ex: CB), attachée à l'utilisateur ci-dessous
+                const methodOfCustomer = await stripe.paymentMethods
+                .attach(
+                "pm_1InPNtLG9PLRTQCELLuwFf96",{
+                    customer: "cus_JPtuAnUxImrvSS",
+                })
+
+                res.json({succes: true, result: methodOfCustomer})
+            } 
+            
+            catch (error) {
+                next(error);
+            }
+
+            
+        },
+        retrieve: async (req, res, next) => {
+            try {
+                const retrieveMethod = await stripe.paymentMethods
+                .retrieve(
+                    "pm_1InOqwLG9PLRTQCE76aBDsHr"
+                )
+
+                res.json({success: true, result: retrieveMethod})
+
+            } catch (error) {
+
+                next(error);
+            }
+        },
+
+        update: async (req, res, next) => {
+            try {
+                const updateMethod = await stripe.paymentMethods
+                .update(
+                    "pm_1InPNtLG9PLRTQCELLuwFf96", { 
+                        billing_details: {
+                            email: "zoldek@sama.fr",
+                            name: "Iuda",
+                            phone: "+33 0707070707",
+                        },
+                    }
+                )
+                
+                res.json({success: true, result: updateMethod})
+            } 
+            
+            catch (error) {
+                
+                next(error);
+            }
+        }
     },
     customers: {
 
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,18 @@ const StripeController = require("./controllers/Stripe");
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
-// app.use(express,urlencoded({ extended : true}));
-// app.use(express.json())
+
+
 //! Intents 
 app.post('/api/intents/create', StripeController.intents.create);
 app.get('/api/intents/retrieve', StripeController.intents.retrieve);
 app.put('/api/intents/update', StripeController.intents.update);
+
+//! Payments
+app.post('/api/payments/create', StripeController.payments.create);
+app.post('/api/payments/saveToUser', StripeController.payments.savePaymentMethodToAnUser);
+app.get('/api/payments/retrieve', StripeController.payments.retrieve);
+app.put('/api/payments/update', StripeController.payments.update);
+
 
 app.listen(5000, () => (console.log("Serveur démaré sur le port 5000")))
