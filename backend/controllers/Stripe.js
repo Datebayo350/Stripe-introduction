06392362f56e37176c2b1a7843a38a6b2ffd134c
ZoldekSama <zoldeksama@gmail.com>
2021-05-04 14:22:07 +0200

    Intents - method : create, created

--- backend/controllers/Stripe.js
+++ backend/controllers/Stripe.js
@@ -1,11 +1,35 @@
 const SECRET_API_KEY = process.env.SECRET_API_KEY;
 const stripe = require('stripe')(SECRET_API_KEY);
 
-
 module.exports = {
     
     intents: {
 
+        create: async (req, res, next) => {
+            try {
+                const paymentIntention = await stripe.paymentIntents
+                .create({
+                    customer:"cus_JQ9oPquYIz7zoN",
+                    amount: "20000",
+                    currency: "eur",
+                    description:" Paiement en cours de réalisation",
+                    receipt_email: "constantin@fitlab.fr",
+                    metadata: {
+                        order_id: "1234",
+                    },
+                })
+
+                return res.json({ success: true, result: paymentIntention })
+            }
+
+            catch (error) {
+
+                next(error);
+            }
+            
+        },
+
+       
     },
 
     payments: {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const StripeController = require("./controllers/Stripe");
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 
+//! Intents 
+app.post('/api/intents/create', StripeController.intents.create);
 
 
 app.listen(5000, () => (console.log("Serveur démaré sur le port 5000")))
