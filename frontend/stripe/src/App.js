f2dbc3a97e10f868c429e53614234b1f0ea62a08
ZoldekSama <zoldeksama@gmail.com>
2021-05-12 00:27:57 +0200

    Stripe Frontend - initial configuration

--- frontend/stripe/src/App.js
+++ frontend/stripe/src/App.js
@@ -1,9 +1,11 @@
+import {loadStripe, Elements} from '@stripe/stripe-js';
 import CheckoutForm from "./components/CheckoutForm";
-
-
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLIC_API_KEY);
 function App() {
   return (
+    <Elements stripe={stripePromise}>
       <CheckoutForm />
+    </Elements>
   );
 }
 
