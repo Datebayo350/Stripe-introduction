c3799b6c1df1e4a1bce9d1cbe3a38830ed47d970
ZoldekSama <zoldeksama@gmail.com>
2021-05-17 17:40:18 +0200

    [Frontend] CustomNavbar Component, improved from the old one

---
+++ frontend/stripe/src/components/CustomNavbar.jsx
@@ -0,0 +1,14 @@
+import "./../App.css"
+import CustomLink from "./CustomLink";
+const CustomNavbar = () => {
+    
+    return (
+        <nav className="mt-3 mx-auto flex justify-evenly font-bold container text-white">
+            <CustomLink path='/'> Home </CustomLink>
+            <CustomLink path='/payment-success'> Success </CustomLink>
+            <CustomLink path='/payment-cancel'> Cancel </CustomLink>
+            <CustomLink path='/checkout-form'> Checkout Form </CustomLink>
+        </nav>
+    )
+}
+export default CustomNavbar;
