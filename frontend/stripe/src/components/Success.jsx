ee7791e91c229027327fc35b008aa9ad3ac65db2
ZoldekSama <zoldeksama@gmail.com>
2021-05-17 17:37:35 +0200

    [Frontend] Success Component, created

---
+++ frontend/stripe/src/components/Success.jsx
@@ -0,0 +1,8 @@
+const Success = () => {
+
+    return (
+        <h1 className="font-bold text-white text-center text-3xl mt-20">Merci pour votre paiement, à très vite dans notre e-boutique !</h1>
+    )
+}
+
+export default Success;
