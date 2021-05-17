06078e1a23fce2f9655b176baa4648d69acdb5be
ZoldekSama <zoldeksama@gmail.com>
2021-05-17 17:36:43 +0200

    [Frontend] Cancel Component, created

---
+++ frontend/stripe/src/components/Cancel.jsx
@@ -0,0 +1,10 @@
+const Cancel = () => {
+    
+    return (
+        
+        <h1 className="font-bold text-white text-center text-3xl mt-20"> Votre paiement a échoué, vous allez être redirigé(e) </h1>
+ 
+    )
+}
+
+export default Cancel;
